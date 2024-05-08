38fa51586f7d0119db327e88e967a3b07892d8a1
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-05-09 01:28:00 +0530

    feat: refactor and update event-carousel
    - remove looping behavior
    - add item limit prop to determine how many items are shown
    - add stretch behavior when items are lesser than limits
    - remove carousel buttons and show overflow on mobile
    - add indicator for disabled buttons
    - remove carousel buttons when there is no overflow of items

--- src/app/event/[id]/components/event-carousel.tsx
+++ src/app/event/[id]/components/event-carousel.tsx
@@ -1,25 +1,42 @@
 import {
   Carousel,
   CarouselContent,
+  CarouselItem,
   type CarouselApi,
 } from "@/components/ui/carousel";
 
 import React, { useEffect } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
-import useIsSmallScreen from "@/components/hooks/useIsSmallScreen";
 import Button from "@/components/Button";
-import { Heading3 } from "@/components/type-styles";
+import { BodyLarge, Heading3 } from "@/components/type-styles";
+import useIsVerySmallScreen from "@/components/hooks/useIsVerySmallScreen";
 
-interface EventCarouselProps{
-  children: React.ReactNode[]
-  title: string
+interface EventCarouselProps {
+  children: React.ReactNode[];
+  title: string;
+  limit?: {
+    sm?: number;
+    lg?: number;
+    xl?: number;
+  };
 }
 
-export default function EventCarousel({children, title} : EventCarouselProps) {
+const defaultLimit = {
+  sm: 2,
+  lg: 3,
+  xl: 3,
+};
+
+export default function EventCarousel({
+  children,
+  title,
+  limit = defaultLimit,
+}: EventCarouselProps) {
   const [api, setApi] = React.useState<CarouselApi>();
+  const length = children.length;
   const [current, setCurrent] = React.useState(0);
   const [count, setCount] = React.useState(0);
-  const isSmallScreen = useIsSmallScreen();
+  const isSmallScreen = useIsVerySmallScreen();
 
   useEffect(() => {
     if (!api) {
@@ -44,41 +61,80 @@ export default function EventCarousel({children, title} : EventCarouselProps) {
 
   return (
     <div className="w-full flex flex-col gap-4 md:gap-4 mt-3">
-      {isSmallScreen && (
-        <div className="flex items-center gap-2">
-          <Button onClick={carouselPrev} variant="primary">
-            <Icon icon="maki:arrow" className="w-auto h-auto rotate-180	" />
-          </Button>
-          <Button onClick={carouselNext} variant="primary">
-            <Icon icon="maki:arrow" className="w-auto h-auto " />
-          </Button>
-        </div>
-      )}
-      {!isSmallScreen && (
-        <div className="max-w-[780px] flex justify-between items-center gap-x-8 gap-y-4">
-          <div className="">
+      <div className="flex justify-between items-center gap-x-8 gap-y-4">
+        <div className="flex items-center gap-3">
           <Heading3>{title}</Heading3>
-          </div>
-          <div className="flex items-center gap-2">
-            <Button onClick={carouselPrev} variant="primary">
+          <BodyLarge className="text-onBackgroundTertiary">{`(${children.length})`}</BodyLarge>
+        </div>
+        {!isSmallScreen && (
+          <div
+            className={`flex items-center gap-2 ${
+              length <= (limit.xl || defaultLimit.xl) ? "xl:hidden" : ""
+            } ${length <= (limit.lg || defaultLimit.lg) ? "lg:hidden" : ""} ${
+              length <= (limit.sm || defaultLimit.sm) ? "sm:hidden" : ""
+            }`}
+          >
+            <Button
+              onClick={carouselPrev}
+              disabled={!api?.canScrollPrev()}
+              variant="secondary"
+            >
               <Icon icon="maki:arrow" className="w-auto h-auto rotate-180	" />
             </Button>
-            <Button onClick={carouselNext} variant="primary">
+            <Button
+              onClick={carouselNext}
+              disabled={!api?.canScrollNext()}
+              variant="secondary"
+            >
               <Icon icon="maki:arrow" className="w-auto h-auto " />
             </Button>
           </div>
-        </div>
-      )}
+        )}
+      </div>
       <Carousel
         opts={{
           align: "start",
-          loop: true,
+          loop: false,
         }}
         className="w-full"
         setApi={setApi}
+        overflow={isSmallScreen}
       >
-        <CarouselContent>{children}</CarouselContent>
-      </Carousel>{" "}
+        <CarouselContent>
+          {children.map((child, index) => (
+            <CarouselItem
+              className={`${
+                limit.sm === 1
+                  ? "sm:basis-full"
+                  : `sm:basis-1/${Math.min(
+                      length,
+                      limit.sm || defaultLimit.sm,
+                      limit.lg || defaultLimit.lg,
+                      limit.xl || defaultLimit.xl
+                    )}`
+              } ${
+                limit.lg === 1
+                  ? "lg:basis-full"
+                  : `lg:basis-1/${Math.min(
+                      length,
+                      limit.lg || defaultLimit.lg,
+                      limit.xl || defaultLimit.xl
+                    )}`
+              } ${
+                limit.xl === 1
+                  ? "xl:basis-full"
+                  : `xl:basis-1/${Math.min(
+                      length,
+                      limit.xl || defaultLimit.xl
+                    )}`
+              }`}
+              key={index}
+            >
+              {child}
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   );
 }
