59286ef8e8faaef69922b0eb5e3568e664fd8314
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-05-10 22:16:05 +0530

    chore(sponsor card): remove repeat image, change button variant

--- src/app/event/[id]/components/sponsor-card.tsx
+++ src/app/event/[id]/components/sponsor-card.tsx
@@ -2,15 +2,15 @@ import Button from "@/components/Button";
 import { Body, Heading3 } from "@/components/type-styles";
 import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import SponsorImage from "@/../public/assets/images/sponsors/sticker-mule.png";
 
 export default function SponsorCard() {
   return (
     <Card className="flex flex-col justify-between bg-surfacePrimary border-borderPrimary overflow-hidden shadow-none">
       <CardHeader className="flex flex-col justify-around">
         <Image
-          src="/images/1635caca9130d9ea879169d6b0025199.png"
+          src={SponsorImage}
           alt={""}
           width={100}
           height={100}
@@ -20,7 +20,7 @@ export default function SponsorCard() {
         <Body className="text-onBackgroundSecondary">Gold Sponsor</Body>
       </CardHeader>
       <CardFooter className=" flex gap-3 ">
-        <Button variant="primary" className="w-full">
+        <Button variant="secondary" className="w-full">
           Learn more
         </Button>
       </CardFooter>
