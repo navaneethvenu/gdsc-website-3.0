38000acafba9aee4831f2d939310d29b127a0f9d
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-06-03 20:59:34 +0530

    chore: update ids for team data

--- src/data/team-members.ts
+++ src/data/team-members.ts
@@ -7,7 +7,7 @@ interface TenureEntry {
 
 export const tenureList: Record<string,  TenureEntry> = {
 
-  "2021-2022":{
+  "21-22":{
   
     description:"Blah Blah blah Blah Blah",
     teamMembers:
@@ -54,7 +54,7 @@ export const tenureList: Record<string,  TenureEntry> = {
 
 },
 
-  "2022-2023":{
+  "23-24":{
 
     description:"The 2021-2022 team was a group of talented individuals who worked together to create a platform that would help students keep their heads in the clouds while having their feet on the ground.",
     teamMembers:[
@@ -97,6 +97,97 @@ export const tenureList: Record<string,  TenureEntry> = {
       },
     },
   ]
+
+},
+"22-23":{
+
+  description:"The 2021-2022 team was a group of talented individuals who worked together to create a platform that would help students keep their heads in the clouds while having their feet on the ground.",
+  teamMembers:[
+  {
+    name: "Ben George Netto",
+    imageURL: "/assets/images/team-photos/bengeorgenetto.png",
+    role: "Visual Designer",
+    team: "Design Team",
+    socials: {
+      twitter: "ritin_george",
+      linkedin: "ritin-george",
+      instagram: "ritin_george",
+      website: "ritin-george.me",
+    },
+  },
+
+  {
+    name: "Ben George Netto",
+    imageURL: "/assets/images/team-photos/bengeorgenetto.png",
+    role: "Visual Designer",
+    team: "Design Team",
+    socials: {
+      twitter: "ritin_george",
+      linkedin: "ritin-george",
+      instagram: "ritin_george",
+      website: "ritin-george.me",
+    },
+  },
+
+  {
+    name: "Ben George Netto",
+    imageURL: "/assets/images/team-photos/bengeorgenetto.png",
+    role: "Visual Designer",
+    team: "Design Team",
+    socials: {
+      twitter: "ritin_george",
+      linkedin: "ritin-george",
+      instagram: "ritin_george",
+      website: "ritin-george.me",
+    },
+  },
+]
+
+}, 
+
+"24-25":{
+
+  description:"The 2021-2022 team was a group of talented individuals who worked together to create a platform that would help students keep their heads in the clouds while having their feet on the ground.",
+  teamMembers:[
+  {
+    name: "Ben George Netto",
+    imageURL: "/assets/images/team-photos/bengeorgenetto.png",
+    role: "Visual Designer",
+    team: "Design Team",
+    socials: {
+      twitter: "ritin_george",
+      linkedin: "ritin-george",
+      instagram: "ritin_george",
+      website: "ritin-george.me",
+    },
+  },
+
+  {
+    name: "Ben George Netto",
+    imageURL: "/assets/images/team-photos/bengeorgenetto.png",
+    role: "Visual Designer",
+    team: "Design Team",
+    socials: {
+      twitter: "ritin_george",
+      linkedin: "ritin-george",
+      instagram: "ritin_george",
+      website: "ritin-george.me",
+    },
+  },
+
+  {
+    name: "Ben George Netto",
+    imageURL: "/assets/images/team-photos/bengeorgenetto.png",
+    role: "Visual Designer",
+    team: "Design Team",
+    socials: {
+      twitter: "ritin_george",
+      linkedin: "ritin-george",
+      instagram: "ritin_george",
+      website: "ritin-george.me",
+    },
+  },
+]
 
 }
 }
